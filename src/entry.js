2ccdaa2880b2a4812142501850a37a6615a72b5c
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-02-01 21:12:13 -0300

    Integrates React

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const node = document.createElement('div')
+
+document.body.append(node)
+
+ReactDOM.render(<div>Hello World</div>, node)
