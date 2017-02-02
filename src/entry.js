cd385860f3cff3b836d5c8f96437fa12d2021256
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-02-01 23:02:22 -0300

    Uses lelao-core with a sample plugin

--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Lelao from 'lelao-core'
 
 const node = document.createElement('div')
 
 document.body.append(node)
 
-ReactDOM.render(<div>Hello World</div>, node)
+ReactDOM.render(
+  <Lelao
+    plugins={{
+      'lelao-plugin-url': require('lelao-plugin-url')
+    }}
+  />,
+  node
+)
