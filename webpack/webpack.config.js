1991a337caa0224e8dcb2608d731bb88ec8c705f
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-02-01 21:08:37 -0300

    Configures babel with React support

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve('dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react', 'stage-0']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Lelao 5rabbits'
